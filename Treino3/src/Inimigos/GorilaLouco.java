78bcff5ac8f01675e28f7bece1484d1ff89b1c76
Gabriel Tavares <140209181+Gabares12@users.noreply.github.com>
2025-08-20 11:21:09 -0300

    Add files via upload

---
+++ Treino3/src/Inimigos/GorilaLouco.java
@@ -0,0 +1,23 @@
+package Inimigos;
+
+public class GorilaLouco extends AtributosIncomun{
+
+    private String nome = "king";
+    private String arma = "punhos";
+    private String armadura = "nao possui";
+    private int vida = 800;
+    private int idade = 15;
+    private int forca = 300 ;
+    private int moedasDeOuro = 15;
+    private double altura = 375 ;
+
+    public void ExibirDado5s(){
+
+
+    }
+
+
+}
+
+//Gorila personagemGorilaLouco = new GoriloLouco();
+
---
+++ Treino3/src/Main.java
@@ -0,0 +1,15 @@
+import Inimigos.ReiDosRatos;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+
+        ReiDosRatos personagemReiDosRatos = new ReiDosRatos();
+        personagemReiDosRatos.ExibirDados();
+
+
+
+
+
+    }
+}
