6ae1ae961616212a6b497a9c012961f31144f122
Gabriel Tavares <140209181+Gabares12@users.noreply.github.com>
2025-08-20 11:51:05 -0300

    Add files via upload

---
+++ Treino3/src/Inimigos/ReiDosRatos.java
@@ -0,0 +1,20 @@
+package Inimigos;
+
+public class ReiDosRatos extends AtributosIncomun{
+
+    private String nome = "ratattui";
+    private String arma ="unhas grandes";
+    private String armadura = "pele Grossa, ";
+    private int vida = 500;
+    private int idade = 250;
+    private int forca = 300;
+    private int moedasDeOuro = 0 ;
+    private double altura = 140;
+
+    }
+
+
+
+
+
+//        ReiDosRatos personagemReiDosRatos = new ReiDosRatos();
