f8bfed6f9fb989fd3849fd50cfb77a546d4edaa8
Gabriel Tavares <140209181+Gabares12@users.noreply.github.com>
2025-08-21 01:18:54 -0300

    Add files via upload

---
+++ DuasClassesSeAtacarem/src/Dois/personagens/Barbaro.java
@@ -0,0 +1,13 @@
+package Dois.personagens;
+
+public class Barbaro extends Personagem {
+
+    public Barbaro(String nome, String arma, int vida, int energia) {
+        super(nome, arma, vida, energia);
+
+    }
+
+    public void atacar(Personagem Vitmia) {
+        int dano = 20;
+    }
+}
