61f7594151a753e01336da6d3f15bc088cc991a4
Gabriel Tavares <140209181+Gabares12@users.noreply.github.com>
2025-08-21 01:12:29 -0300

    Add files via upload

---
+++ Treino3/src/Inimigos/DuasClassesSeAtacarem/src/Dois/personagens/Cavaleiro.java
@@ -0,0 +1,15 @@
+package Dois.personagens;
+
+public class Cavaleiro extends Personagem {
+
+    public Cavaleiro(String nome, String arma, int vida, int energia) {
+        super(nome, arma, vida, energia);
+    }
+
+    public void atacar(Personagem Vitmia) {
+        int dano = 15;
+    }
+
+}
+
+
