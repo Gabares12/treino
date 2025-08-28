6d33a75a0798ce75f37f837e8b2b1f31333e3414
Gabriel Tavares <140209181+Gabares12@users.noreply.github.com>
2025-08-28 11:53:43 -0300

    Add files via upload

---
+++ Treino 4/src/Main.java
@@ -0,0 +1,20 @@
+
+import SalasDeAula.SalaTres;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        SalaTres SalaTres = new SalaTres();
+        SalaTres.exibirConteudo();
+
+    }
+}
+
+
+
+//SalaUm SalaUm = new SalaUm();
+//        SalaUm.exibirConteudo();
+
+//SalaDois salaDois = new SalaDois();
+//        salaDois.exibirConteudo();
