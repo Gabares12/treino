1c6cf735c36a47bec053de0e513f96c738c0a249
Gabriel Tavares <140209181+Gabares12@users.noreply.github.com>
2025-09-02 11:30:53 -0300

    Update Fred.java

--- fred faz bear/src/Animatronics/Fred.java
+++ fred faz bear/src/Animatronics/Fred.java
@@ -1,4 +1,19 @@
 package Animatronics;
 
-public class Fred {
+public class Fred extends Animatronics{
+
+public Fred(){
+    setNomes("Fred");
+    setCores("Marrom");
+    setItem("Microfone");
+    setLocal("Palco");
+    setDataDeFabricacao(1983);
+
+
+}
+
+    @Override
+    public void exbirConteudo() {
+
+    }
 }
