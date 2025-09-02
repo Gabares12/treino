abd92d46063dc65aa950a7b325bf1700f49cc58f
Gabriel Tavares <140209181+Gabares12@users.noreply.github.com>
2025-09-02 11:31:41 -0300

    Update Banheiros.java

--- fred faz bear/src/Banheiros/Banheiros.java
+++ fred faz bear/src/Banheiros/Banheiros.java
@@ -1,20 +1,55 @@
 package Banheiros;
 
-public class Banheiros {
+public abstract class Banheiros{
 
-    private String descricaoBanheiro;
+    private int cameras;
+    private String espelho;
+    private int vasos;
+    private int pia;
+
+    public int getCameras() {
+        return cameras;
+    }
+
+    public void setCameras(int cameras) {
+        this.cameras = cameras;
+    }
+
+    public String getEspelho() {
+        return espelho;
+    }
+
+    public void setEspelho(String espelho) {
+        this.espelho = espelho;
+    }
+
+    public int getVasos() {
+        return vasos;
+    }
+
+    public void setVasos(int vasos) {
+        this.vasos = vasos;
+    }
+
+    public int getPia() {
+        return pia;
+    }
+
+    public void setPia(int pia) {
+        this.pia = pia;
+    }
+
+    public abstract void exibirConteudo();
 }
-
-
-
-
-
-
-
-
-
 
 
 
 //private int vasos;
 //private int pia;
+
+
+
+//public class Banheiros {
+//
+//    private String descricaoBanheiro;
+//}
