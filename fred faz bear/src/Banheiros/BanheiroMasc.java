fd05a4b918b536d941f88f6f3616b526cd530e06
Gabriel Tavares <140209181+Gabares12@users.noreply.github.com>
2025-09-02 11:32:07 -0300

    Update BanheiroMasc.java

--- fred faz bear/src/Banheiros/BanheiroMasc.java
+++ fred faz bear/src/Banheiros/BanheiroMasc.java
@@ -1,4 +1,29 @@
 package Banheiros;
 
-public class BanheiroMasc {
+public class BanheiroMasc extends Banheiros{
+
+    public BanheiroMasc(){
+
+        setVasos(4);
+        setPia(4);
+        setEspelho("espelho");
+        setCameras(1);
+
+
+    }
+    @Override
+    public void exibirConteudo() {
+        System.out.println("quando voce entra na camera do banheiro");
+        System.out.println("voce enxerga paredes e pisos quadriculadas nas cores preto e branco");
+        System.out.println("quase como um tabuleiro de xadres ");
+        System.out.println("voce enxerga 4 cabines  ");
+        System.out.println("duas delas sem porta ");
+        System.out.println("alem disso voce tambem enxerga 4 pias");
+        System.out.println("");
+
+
+
+
+    }
 }
+
